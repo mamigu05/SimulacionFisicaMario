bffd0cf7fad3c16d2c7d6d5ce7d1c6d51bb402f0
mamigu05 <mamigu05@ucm.es>
2022-10-21 00:00:03 +0200

    ParticleSystem

--- skeleton/Particle.cpp
+++ skeleton/Particle.cpp
@@ -1,13 +1,15 @@
 #include "Particle.h"
 
-Particle::Particle(Vector3 pos, Vector3 vel, Vector3 acc, double damp, double size, Vector4 color)
+Particle::Particle(Vector3 pos, Vector3 vel, Vector3 acc, double damp, double size, Vector4 color, double time)
 {
 	v = vel;
 	a = acc;
 	damping = damp;
+	_remaining_time = time;
 
 	pose = physx::PxTransform(pos.x, pos.y, pos.z);
 	renderItem = new RenderItem(CreateShape(physx::PxSphereGeometry(size)), &pose, color);
+	death = GetCurrentTime() + time;
 }
 
 Particle::~Particle()
@@ -20,4 +22,29 @@ void Particle::update(double t)
 	pose = physx::PxTransform(pose.p.x + v.x * t, pose.p.y + v.y * t, pose.p.z + v.z * t);
 	v += a * t;
 	v *= powf(damping, t);
+}
+
+ParticleSystem::ParticleSystem()
+{
+
+}
+
+void ParticleSystem::update(double t)
+{
+	v = Vector3((rand() % 72 - 36), 30, (rand() % 72 - 36));
+
+	_particles.push_back(new Particle(Vector3(0.0, 0.0, 0.0), v, Vector3(0, -9.8, 0), 0.99, 1.0, { 1.0, 1.0, 0.0, 1.0 }, 50));
+
+	for (Particle* p : _particles) {
+		p->update(t);
+		if (p->pose.p.y < -10)
+			_particlesToDelete.emplace_back(p);
+	}
+
+	for (Particle* p : _particlesToDelete) {
+		_particles.remove(p);
+		delete p;
+	}
+
+	_particlesToDelete.clear();
 }
--- skeleton/Particle.h
+++ skeleton/Particle.h
@@ -3,18 +3,22 @@
 #include "RenderUtils.hpp"
 #include <list>
 
+using namespace std;
+
 class Particle
 {
 private:
-	physx::PxTransform pose;
 	RenderItem* renderItem;
 
 	Vector3 v;
 	Vector3 a;
 
 	double damping;
+	
+	double _remaining_time, death;
 public:
-	Particle(Vector3 pos, Vector3 vel, Vector3 acc, double damping, double size, Vector4 color);
+	physx::PxTransform pose;
+	Particle(Vector3 pos, Vector3 vel, Vector3 acc, double damping, double size, Vector4 color, double time);
 	~Particle();
 
 	void update(double t);
@@ -24,10 +28,13 @@ public:
 class ParticleSystem
 {
 protected:
-	std::list<Particle*> _particles;
+	Vector3 v;
+	list<Particle*> _particles;
+	list<Particle*> _particlesToDelete;
 	//std::list<ParticleGenerator*> _particleGenerators;
 	Vector3 _gravity;
 public:
+	ParticleSystem();
 	void update(double t);
 	//ParticleGenerator* getParticleGenerator(string name);
 	void generateFireworkSystem();
